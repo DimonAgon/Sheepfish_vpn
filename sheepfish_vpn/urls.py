efe2fb75d8ae333256117d58939f0af42e115e52
DimonAgon <dimon2003agon@gmail.com>
2023-11-15 16:25:51 +0200

    add vpnsite view, template, url, configure settings

--- sheepfish_vpn/urls.py
+++ sheepfish_vpn/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from vpnsite.views import vpnsite
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('vpnsite/', vpnsite, name='vpnsite'),
 ]
--- vpnsite/views.py
+++ vpnsite/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def vpnsite(request):
+    return render(request, 'vpnsite.html')
