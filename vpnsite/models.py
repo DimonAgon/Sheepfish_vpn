10d4914bd37913bbaa092c2528c6f6fccdf1a211
DimonAgon <dimon2003agon@gmail.com>
2023-11-18 01:17:42 +0200

    add statistics, statistics show mechanism

--- vpnsite/models.py
+++ vpnsite/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
-
+class Statistics(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    transitions = models.IntegerField(verbose_name="site transitions", default=0)
+    volume = models.FloatField(verbose_name="data volume", default=0)
--- vpnsite/views.py
+++ vpnsite/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, redirect
 
+from django.contrib.auth.models import User
+
+from vpnsite.models import Statistics
+
 
 def vpnsite(request):
     if request.user.is_authenticated:
-        return render(request, 'vpnsite.html')
+        user_object = User.objects.get(username=request.user)
+        statistics, __ = Statistics.objects.get_or_create(user=user_object)
+        context = {'user': user_object,
+                   'statistics': statistics}
+        return render(request, 'vpnsite.html', context=context)
 
     else:
         return redirect('authorization')
