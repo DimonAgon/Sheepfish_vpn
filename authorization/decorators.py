4f9192b144bd186f492efa3160833b61f0c20171
DimonAgon <dimon2003agon@gmail.com>
2023-11-20 11:09:28 +0200

    add message to redirect-unauthorized-user decorator

--- authorization/decorators.py
+++ authorization/decorators.py
@@ -2,6 +2,9 @@ from functools import wraps
 
 from django.shortcuts import redirect
 
+from django.contrib import messages
+from authorization.static_text.static_text import *
+
 
 def redirect_unauthorized_users(user_function):
     @wraps(user_function)
@@ -10,5 +13,6 @@ def redirect_unauthorized_users(user_function):
             return user_function(request, *args, **kwargs)
 
         else:
+            messages.error(request, authorization_suggestion_error_message)
             return redirect('authorization')
     return wrap
--- authorization/static_text/static_text.py
+++ authorization/static_text/static_text.py
@@ -2,6 +2,9 @@
 account_created_success_message = "an account created"
 account_has_not_been_created__invalid_data__error_message = "Invalid data, an account has not been created"
 
+
 #authorization
 log_in_success_message = "logged in"
 log_in__invalid_data__error_message = "Invalid data, cannot log in"
+
+authorization_suggestion_error_message = "Please authorize first"
