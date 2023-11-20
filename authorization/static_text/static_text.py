4179c816187dffce5e4be10f905b2a926d688cb4
DimonAgon <dimon2003agon@gmail.com>
2023-11-20 02:02:09 +0200

    remove ip info from authorization messages

--- authorization/static_text/static_text.py
+++ authorization/static_text/static_text.py
@@ -1,7 +1,7 @@
 #registration
-account_created_success_message = "an account created for {}"
-account_has_not_been_created__invalid_data__error_message = "Invalid data, an account has not been created for {}"
+account_created_success_message = "an account created"
+account_has_not_been_created__invalid_data__error_message = "Invalid data, an account has not been created"
 
 #authorization
-log_in_success_message = "log in from {}"
-log_in__invalid_data__error_message = "Invalid data, cannot log in from {}"
+log_in_success_message = "logged in"
+log_in__invalid_data__error_message = "Invalid data, cannot log in"
--- authorization/views.py
+++ authorization/views.py
@@ -7,21 +7,18 @@ from authorization.forms import NativeUserCreationForm, NativeAuthenticationForm
 from authorization.infrastructure.enums import *
 from authorization.static_text.static_text import *
 
-from ipware import get_client_ip
-
 
 def register(request):
     if request.method == 'POST':
         form = NativeUserCreationForm(request.POST)
-        client_ip = get_client_ip(request)[0]
         if form.is_valid():
             form.save()
-            messages.success(request, account_created_success_message.format(client_ip))
+            messages.success(request, account_created_success_message)
             return redirect('vpnsite')
 
         else:
             messages.error(
-                request, account_has_not_been_created__invalid_data__error_message.format(client_ip)
+                request, account_has_not_been_created__invalid_data__error_message
             )
 
     else:
@@ -32,15 +29,14 @@ def register(request):
 def authorize(request):
     if request.method == 'POST':
         form = NativeAuthenticationForm(data=request.POST)
-        client_ip = get_client_ip(request)[0]
         if form.is_valid():
             user = form.get_user()
             login(request, user)
-            messages.success(request, log_in_success_message.format(client_ip))
+            messages.success(request, log_in_success_message)
             return redirect('vpnsite')
 
         else:
-            messages.error(request, log_in__invalid_data__error_message.format(client_ip))
+            messages.error(request, log_in__invalid_data__error_message)
 
     else:
         form = NativeAuthenticationForm()
